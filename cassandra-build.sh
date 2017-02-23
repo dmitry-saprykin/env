f139eee44d5253de43a64fc50e865d212bb6b3ee
Dmitrii Saprykin <dmitrii.saprykin@nih.gov>
2017-02-23 15:01:01 -0500

    * added git clean for cassandra

--- cassandra-build.sh
+++ cassandra-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash                                                                                         
 #export JAVA_HOME=~/jdk1.7.0_75/; export ANT_HOME=~/apache-ant-1.9.4/; ~/apache-ant-1.9.4/bin/ant --execdebug
                                                                                                     
-                                                                                                    
+git clean -xfd
+
 unset JAVA_TOOL_OPTIONS                                                                             
 export LANG="en_US.UTF-8"                                                                           
 export JAVA_TOOL_OPTIONS="-Dfile.encoding=UTF8"                                                     
